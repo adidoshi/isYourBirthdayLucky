14b954cbcf42c037d6f9b55baef95b7ea1996402
adidoshi <adityadoshi25@gmail.com>
2021-08-09 02:53:34 +0530

    initial commit
    Uploading all files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lucky-Birthday
+
+This is fun app, created using pure Vanilla JS.
+
+How this works? If sum of digits in your birthdate is divisible by your lukcy number, then your birthday is lucky.
+
+How to use? Enter your birthdate & your lucky number & the app will show your birthdate is lucky or not.
+
+Privacy Notice: We do not store or share your personal data and information.
---
+++ app.js
@@ -0,0 +1,32 @@
+const birthDate = document.querySelector("#birthDate");
+const luckyNumber = document.querySelector("#luckyNum");
+const checkBtn = document.querySelector("#checkBtn");
+const result = document.querySelector(".result");
+
+const privacyNotice = document.querySelector(".privacyNote")
+const close = document.querySelector("#close");
+
+close.addEventListener('click', () => {
+    privacyNotice.style.display = "none";
+})
+
+checkBtn.addEventListener("click", checkClickHandler);
+
+function checkClickHandler(event) {
+    event.preventDefault();
+    let newString = birthDate.value.split("-").join("");
+    let sum = 0;
+    for (digit of newString) {
+        sum += +digit;
+    }
+    const luckyNumJS = +luckyNumber.value;
+    if (sum % luckyNumJS === 0) {
+        result.innerHTML = `Hurray!! You are a lucky person!
+          <img src = "luckygif.gif">
+          `;
+    } else {
+        result.innerHTML = `Oops!! Your birthday is not a lucky number!
+          <img id="size" src = "unlucky.gif">
+          `;
+    }
+}
